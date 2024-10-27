88c81467ec9f14700c91780cb452fedb48dd784f
Sergey Dolin <sergey@s4y.solutions>
2024-10-27 17:17:18 +0100

    Add svg icons to bottom nav bar

--- flutter/lib/presentation/scaffolds/bottom_nav.dart
+++ flutter/lib/presentation/scaffolds/bottom_nav.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_l10n.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
 
 class BottomNavScaffold extends StatelessWidget {
@@ -9,37 +10,61 @@ class BottomNavScaffold extends StatelessWidget {
   const BottomNavScaffold(
       {super.key, required this.child, required this.state});
 
+  static ColorFilter? itemColor(BuildContext context, int buttonIndex, int currentIndex) {
+    final color = currentIndex == buttonIndex
+        ? Theme.of(context).bottomNavigationBarTheme.selectedItemColor
+        : Theme.of(context).bottomNavigationBarTheme.unselectedItemColor;
+    return color != null ? ColorFilter.mode(color, BlendMode.srcIn) : null;
+  }
+
   @override
   Widget build(BuildContext context) {
     final l10n = AppLocalizations.of(context)!;
     final backgroundColor =
         Theme.of(context).bottomNavigationBarTheme.backgroundColor;
+    final currentIndex = state.uri.path == '/home'
+        ? 0
+        : state.uri.path == '/chats'
+            ? 1
+            : state.uri.path == '/favorites'
+                ? 2
+                : 3;
+
+
     return Scaffold(
       body: child,
       bottomNavigationBar: _bottom()
           ? BottomNavigationBar(
-              currentIndex: state.uri.path == '/home'
-                  ? 0
-                  : state.uri.path == '/chats'
-                      ? 1
-                      : state.uri.path == '/favorites'
-                          ? 2
-                          : 3,
+              currentIndex: currentIndex,
               items: [
                 BottomNavigationBarItem(
-                    icon: const Icon(Icons.home),
+                    icon: SvgPicture.asset('assets/images/bottombar/home.svg',
+                        width: 24,
+                        height: 24,
+                        colorFilter: itemColor(context, 0, currentIndex)),
                     label: l10n.homeLabel,
                     backgroundColor: backgroundColor),
                 BottomNavigationBarItem(
-                    icon: const Icon(Icons.chat),
+                    icon: SvgPicture.asset('assets/images/bottombar/chats.svg',
+                        width: 24,
+                        height: 24,
+                        colorFilter: itemColor(context, 1, currentIndex)),
                     label: l10n.chatsLabel,
                     backgroundColor: backgroundColor),
                 BottomNavigationBarItem(
-                    icon: const Icon(Icons.favorite),
+                    icon: SvgPicture.asset(
+                        'assets/images/bottombar/favorites.svg',
+                        width: 24,
+                        height: 24,
+                        colorFilter: itemColor(context, 2, currentIndex)),
                     label: l10n.favoritesLabel,
                     backgroundColor: backgroundColor),
                 BottomNavigationBarItem(
-                    icon: const Icon(Icons.settings),
+                    icon: SvgPicture.asset(
+                        'assets/images/bottombar/settings.svg',
+                        width: 24,
+                        height: 24,
+                        colorFilter: itemColor(context, 3, currentIndex)),
                     label: l10n.settingsLabel,
                     backgroundColor: backgroundColor),
               ],
