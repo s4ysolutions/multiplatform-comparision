ebdf17c27a79782e4eb9aef6f119d1d450790cde
Sergey Dolin <sergey@s4y.solutions>
2024-10-31 13:24:04 +0100

    Upgrade targetSDK to 35

--- kotlin/app/build.gradle.kts
+++ kotlin/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "solutions.s4y.puredemo"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
