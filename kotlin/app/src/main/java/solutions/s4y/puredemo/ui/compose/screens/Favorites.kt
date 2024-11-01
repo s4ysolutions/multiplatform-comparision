e5e13e857ec1594a962bb8665d0e7751577f5330
Sergey Dolin <sergey@s4y.solutions>
2024-11-01 07:37:25 +0100

    Add Screens

---
+++ kotlin/app/src/main/java/solutions/s4y/puredemo/ui/compose/screens/Favorites.kt
@@ -0,0 +1,10 @@
+package solutions.s4y.puredemo.ui.compose.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import solutions.s4y.puredemo.R
+
+@Composable
+fun FavoritesScreen() {
+    Greeting(stringResource(id = R.string.favorites));
+}
