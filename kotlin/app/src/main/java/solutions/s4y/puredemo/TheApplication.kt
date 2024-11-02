a819800e77cf71eff53dc3abfd744d7275fa25dd
Sergey Dolin <sergey@s4y.solutions>
2024-11-02 07:15:09 +0100

    Remove Consumer component

--- kotlin/app/src/main/java/solutions/s4y/puredemo/TheApplication.kt
+++ kotlin/app/src/main/java/solutions/s4y/puredemo/TheApplication.kt
@@ -8,13 +8,10 @@ import javax.inject.Inject
 class TheApplication: Application() {
     private lateinit var _appComponent: ApplicationComponent
     val appComponent get() = _appComponent
-    @Inject
-    lateinit var consumer: Consumer
 
     override fun onCreate() {
         super.onCreate()
         _appComponent = DaggerApplicationComponent.factory().create(applicationContext)
         appComponent.inject(this)
-        consumer.consume()
     }
 }
