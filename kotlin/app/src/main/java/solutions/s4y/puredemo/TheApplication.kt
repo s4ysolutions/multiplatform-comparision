71633bccdeeba742a6ec54a1075762ac45d07e32
Sergey Dolin <sergey@s4y.solutions>
2024-11-05 10:20:20 +0100

    Make application open

--- kotlin/app/src/main/java/solutions/s4y/puredemo/TheApplication.kt
+++ kotlin/app/src/main/java/solutions/s4y/puredemo/TheApplication.kt
@@ -4,19 +4,13 @@ import android.app.Application
 import solutions.s4y.puredemo.dagger.ApplicationComponent
 import solutions.s4y.puredemo.dagger.DaggerApplicationComponent
 
-class TheApplication: Application() {
+open class TheApplication: Application() {
     private lateinit var _appComponent: ApplicationComponent
     val dagger get() = _appComponent
 
     override fun onCreate() {
         super.onCreate()
         initDagger()
-        /*
-        _appComponent = DaggerComponentApplication.factory().create(applicationContext)
-        // appComponent.inject(this)
-        Log.d(TAG, appComponent.getWarrior().name)
-        Log.d(TAG, appComponent.getWarrior().name)
-         */
     }
 
     protected fun initDagger() {
